9f171dd0eb3d615db49f64e7cdbfe92ba56cb2cb
Alistair Chapman <alistair.chapman@connect.qut.edu.au>
2016-01-20 02:22:50 +1000

    Minor changes

--- src/TestApp/Program.cs
+++ src/TestApp/Program.cs
@@ -10,8 +10,10 @@ namespace TestApp
     internal class Program
     {
         private static string DefaultProjectPath { get; set; } =
-            @"C:\Users\UCRM4\Source\ACN\TFSShowcase\src\ScreenshotReportCreator\ScreenshotReportCreator.csproj";
-        //@"C:\Users\alist_000\Source\ACN\TFSShowcase\src\ScreenshotReportCreator\ScreenshotReportCreator.csproj";
+            //@"C:\Users\UCRM4\Source\ACN\TFSShowcase\src\ScreenshotReportCreator\ScreenshotReportCreator.csproj"; // ATO
+            //@"C:\Users\alist_000\Source\ACN\TFSShowcase\src\ScreenshotReportCreator\ScreenshotReportCreator.csproj"; //others
+            //@"C:\Users\alist\Source\ACN\TFSShowcase\src\ScreenshotReportCreator\ScreenshotReportCreator.csproj"; // Zenbook
+            @"C:\Users\alist_000\Source\ACN\myTaxFramework\FormDocuments\DocumentConversion\DocumentConversion.csproj";
 
         private static void Main(string[] args)
         {
