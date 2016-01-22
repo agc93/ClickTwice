6a2759dd144c44f5e1b0d36fea6447e46bc65195
Alistair Chapman <alistairgchapman@gmail.com>
2016-01-23 02:21:28 +1000

    Basic template plumbing

---
+++ src/ClickTwice.Handlers.LaunchPage/TemplateSource.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClickTwice.Handlers.LaunchPage
+{
+    class TemplateSource
+    {
+    }
+}
