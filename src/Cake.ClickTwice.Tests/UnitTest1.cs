c9bae5e5c245570d0952f3165ae6411e2e7bb39a
Alistair Chapman <alistair.chapman@connect.qut.edu.au>
2016-05-25 13:59:32 +1000

    Add build output logger for unit tests

--- src/Cake.ClickTwice.Tests/UnitTest1.cs
+++ src/Cake.ClickTwice.Tests/UnitTest1.cs
@@ -4,16 +4,25 @@ using Cake.Core.IO;
 using Cake.Testing;
 using ClickTwice.Publisher.Core;
 using ClickTwice.Publisher.Core.Handlers;
+using ClickTwice.Publisher.Core.Loggers;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Cake.ClickTwice.Tests
 {
     public class UnitTest1
     {
+        public UnitTest1(ITestOutputHelper helper)
+        {
+            Output = helper;
+        }
+
+        private ITestOutputHelper Output { get; set; }
+
         [Fact]
         public void CakeAliasTest()
         {
-            var fixture = new ClickTwiceFixture();
+            var fixture = new ClickTwiceFixture(Output);
             var mgr =
                 fixture.Run(
                     @"C:\Users\alist\Source\ACN\myTaxFramework\FormDocuments\DocumentConversion\DocumentConversion.csproj");
@@ -28,24 +37,49 @@ namespace Cake.ClickTwice.Tests
 
     public class ClickTwiceFixture
     {
-        public ClickTwiceFixture()
+        public ClickTwiceFixture(ITestOutputHelper output)
         {
-            var environment = FakeEnvironment.CreateUnixEnvironment();
+            var environment = FakeEnvironment.CreateWindowsEnvironment();
             Environment = environment;
             var fileSystem = new FakeFileSystem(environment);
             fileSystem.CreateDirectory("./artifacts");
             //fileSystem.CreateFile(PackageFilePath.FullPath).SetContent(SamplePackageJson);
             FileSystem = fileSystem;
+            Logger = new UnitTestLogger(output);
         }
 
         public IFileSystem FileSystem { get; set; }
 
         public ICakeEnvironment Environment { get; set; }
+        private IPublishLogger Logger { get; }
 
         public ClickTwiceManager Run(FilePath projectFile)
         {
             var manager = new ClickTwiceManager(projectFile, new FakeLog());
+            manager.LogTo(Logger);
             return manager;
+        }
+    }
+
+    public class UnitTestLogger : IPublishLogger
+    {
+        public UnitTestLogger(ITestOutputHelper helper)
+        {
+            Logger = helper;
+            IncludeBuildMessages = true;
+        }
+
+        private ITestOutputHelper Logger { get; set; }
+
+        public void Log(string content)
+        {
+            Logger.WriteLine(content);
+        }
+
+        public bool IncludeBuildMessages { get; set; }
+        public string Close(string outputPath)
+        {
+            throw new NotImplementedException();
         }
     }
 }
